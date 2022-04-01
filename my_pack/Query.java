2adf53596f54a5adaba92190058b2cc1aab192c4
adhokshaja.acharya@mca.christuniversity.in <95070489+Adhokshaja73@users.noreply.github.com>
2022-04-01 14:31:26 +0530

    beautify-table-output

--- my_pack/Query.java
+++ my_pack/Query.java
@@ -1,7 +1,63 @@
 package my_pack;
 
+import java.util.ArrayList;
+
 public class Query {
     public void hello() {
         System.out.println("Hello");
+    }
+
+    public static String beautify(ArrayList<ArrayList<String>> result) {
+        int maxLen = getMaxLen(result);
+        StringBuilder newResult = new StringBuilder();
+
+        newResult.append("\n+" + getBorder(maxLen * result.get(0).size() + 2 * (result.get(0).size())) + "+\n| ");
+        ArrayList<String> currentRow = result.get(0);
+        for (int j = 0; j < currentRow.size(); j += 1) {
+            String currentString = currentRow.get(j);
+            newResult.append(currentString);
+            for (int k = 0; k <= maxLen - currentString.length(); k += 1)
+                newResult.append(" ");
+            newResult.append("|");
+        }
+        newResult.append("\n+" + getBorder(maxLen * result.get(0).size() + 2 * (result.get(0).size())) + "+\n| ");
+
+        for (int i = 1; i < result.size(); i += 1) {
+            currentRow = result.get(i);
+            for (int j = 0; j < currentRow.size(); j += 1) {
+                String currentString = currentRow.get(j);
+                newResult.append(currentString);
+                for (int k = 0; k <= maxLen - currentString.length(); k += 1)
+                    newResult.append(" ");
+                newResult.append("|\n| ");
+
+            }
+        }
+        newResult.replace(newResult.length() - 3, newResult.length(), "");
+        newResult.append("\n+" + getBorder(maxLen * result.get(0).size() + 2 * (result.get(0).size())) + "+\n");
+
+        String stringRes = new String(newResult);
+        return stringRes;
+    }
+
+    private static String getBorder(int size) {
+        StringBuffer outputBuffer = new StringBuffer(size);
+        for (int i = 0; i < size; i++) {
+            outputBuffer.append("-");
+        }
+        return outputBuffer.toString();
+    }
+
+    private static int getMaxLen(ArrayList<ArrayList<String>> result) {
+        int maxLen = 0;
+        for (int i = 0; i < result.size(); i += 1) {
+            ArrayList<String> currentRow = result.get(i);
+            for (int j = 0; j < currentRow.size(); j += 1) {
+                if (maxLen < currentRow.get(j).length()) {
+                    maxLen = currentRow.get(j).length();
+                }
+            }
+        }
+        return maxLen;
     }
 }
--- my_pack/QueryParser.java
+++ my_pack/QueryParser.java
@@ -6,31 +6,38 @@ public class QueryParser {
     public ArrayList<Database> databaseList;
     public Database currentDatabase;
 
-    public Query query;
+    public Query query = new Query();
 
     public QueryParser(ArrayList<Database> databaseList) {
         this.databaseList = databaseList;
     }
 
     public String parse(String mQuery) {
-        String result = "TEST";
+        String result = "test";
         mQuery = mQuery.substring(1, mQuery.length() - 1);
+
         String[] words = mQuery.split(" ");
-        if (words[0].equals("CREATE") && words[1].equals("DATABASE") && words.length == 3) {
+
+        if (mQuery.matches("CREATE DATABASE [A-Za-z0-9]+")) {
             String newDbName = words[2];
             Database newDb = new Database(newDbName, new ArrayList<Table>());
             Server.databaseList.add(newDb);
             Server.currentDatabase = newDb;
             Server.dbMap.put(newDbName, newDb);
             result = "Databse added : " + newDb.databaseName;
-        } else if (words[0].equals("SHOW") && words[1].equals("DATABASE") && words.length == 2) {
-            String dbName = " ";
-
+        } else if (mQuery.matches("SHOW DATABASE") &&
+                words.length == 2) {
+            ArrayList<ArrayList<String>> dbList = new ArrayList<>();
+            ArrayList<String> title = new ArrayList<>();
+            title.add("Databases");
+            dbList.add(title);
             for (int i = 0; i < Server.databaseList.size(); i += 1) {
-                dbName += "\n" + Server.databaseList.get(i).databaseName;
+                ArrayList<String> temp = new ArrayList<>();
+                temp.add(Server.databaseList.get(i).getDatabaseName());
+                dbList.add(temp);
             }
-            result = dbName;
-        } else if (words[0].equals("USE") && words.length == 2) {
+            result = Query.beautify(dbList);
+        } else if (mQuery.matches("USE [A-Za-z0-9]+")) {
             try {
                 Server.currentDatabase = Server.dbMap.get(words[1]);
                 result = "SELECTED DATBASE " + Server.currentDatabase.databaseName;
@@ -41,6 +48,7 @@ public class QueryParser {
         } else {
             result = "Invalid input";
         }
+
         return result;
     }
 }
